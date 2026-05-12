0bfd25c37233f656ba8a7bd77649f7650cb7f1fd
necro1024 <corzoricardo130@gmail.com>
2026-05-11 22:09:53 -0500

    Delete app-backend directory

--- app-backend/src/main/java/backend/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package backend.repository;
-
-import backend.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-}
