17c341b3b4303900c90fbeacb62123d039d81c6a
0Geonemesis <mathius-mb@hotmail.com>
2026-06-09 13:21:36 -0500

    subida de ultima hora carpeta

--- app-frontend/src/pages/Login.jsx
+++ app-frontend/src/pages/Login.jsx
@@ -1,4 +1,4 @@
-import Navbar from "../components/Navbar";
+import Navbar from "../components/common/Navbar.jsx";
 import { Link } from "react-router-dom";
 
 function Login() {
--- app-frontend/src/pages/SolicitarPrestamo.jsx
+++ app-frontend/src/pages/SolicitarPrestamo.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import Navbar from "../components/Navbar";
+import Navbar from "../components/common/Navbar.jsx";
 
 function SolicitarPrestamo() {
     const [monto, setMonto] = useState("");
